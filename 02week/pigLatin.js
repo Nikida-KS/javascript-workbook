931b664b78624451e65252da3c637c1134e995f4
Nikida Sexton <nikida.ks@gmail.com>
2018-07-25 17:26:55 -0500

    wrote code plan and followed it

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,10 +7,85 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+/*
+pig latin code plan
+
+0. Write a function to check for valid input
+  1. Check that input is a string
+  2. Check for other things such as
+
+
+1. Write a function that takes in the word and returns the word as a lower case, whitespace-trimmed word
+
+2. Write a function that checks for a vowel
+
+
+3. Write a function that attaches "yay" to the end of a string
+
+
+4. Write a function that:
+ 1. splits the string at the first vowel
+ 2. stores the first item and deletes it from the array
+ 3. pops that item to the end
+ 4. pops "ay" to the end
+ 5. joins the array as a string
+ 6. returns the string
+
+5. Write the parent code to organize pure codes
+
+
+*/
+const vowelStartYayEnd= (word) => {
+  //checks if anything in the array (vowels) is in the first position (first letter) of the word. This method returns -1 if it doesn't match anything in the array
+  if(["a", "e", "i", "o", "u"].indexOf(word[0]) > -1){
+    return word = word + "yay"
+  }
+  else{
+    return consonantStartAyEnd(word);
+  }
+}
+
+const consonantStartAyEnd = (word) => {
+  //for loop finds the location of the first vowel
+  for (var i = 0; i < word.length; i++){
+    if(["a", "e", "i", "o", "u"].indexOf(word[i]) > -1){
+      //moveConsonants saves the string from position 0 to the position of the first vowel
+      let moveConsonants = word.slice(0,i);
+      //chunk saves the string from the position of the first vowel to the end of the string
+      let chunk = word.slice(i, word.length);
+      //puts all of the pieces together
+      word = chunk + moveConsonants + "ay";
+    return word;
+    }
+    else{
+     return word = word + "ay"
+   }
+  }
+}
+
+const isValid = (word) => {
+  //lowercases all of the letters and trims whitespace from the front or the back
+  word = word.toLowerCase().trim();
+  //checks if string includes a space, hyphen or underscore
+  if(word.includes(" ")||word.includes("-") ||word.includes("_")){
+    return "This game only works one word at a time"
+  }
+  else{
+  //makes sure the word starts with a letter
+  for (var i = 0; i < word.length; i++){
+      if(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"].indexOf(word[0]) > -1){
+      return vowelStartYayEnd(word);
+      }
+      else{
+        return "You could at least start your weird word with a letter...."
+      }
+    }
+  }
+}
 
 function pigLatin(word) {
 
-  // Your code here
+  return isValid(word)
 
 }
 
