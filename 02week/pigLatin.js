f17ffa3aab269f44f9887507042e035bff857540
Nikida Sexton <nikida.ks@gmail.com>
2018-07-31 18:58:28 -0500

    cleaned up code

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -39,30 +39,29 @@ caseSpace= (word) => {
  6. returns the string
 
 5. Write the parent code to organize pure codes
-
-
 */
-const vowelStartYayEnd= (word) => {
+
+
+const vowelStartYayEnd = (word) => {
   //checks if anything in the array (vowels) is in the first position (first letter) of the word. This method returns -1 if it doesn't match anything in the array
-  if(["a", "e", "i", "o", "u"].indexOf(word[0]) > -1){
+  if (["a", "e", "i", "o", "u"].indexOf(word[0]) > -1) {
     return word = word + "yay"
-  }
-  else{
+  } else {
     return consonantStartAyEnd(word);
   }
 }
 
 const consonantStartAyEnd = (word) => {
   //for loop finds the location of the first vowel
-  for (var i = 0; i < word.length; i++){
-    if(["a", "e", "i", "o", "u"].indexOf(word[i]) > -1){
+  for (var i = 0; i < word.length; i++) {
+    if (["a", "e", "i", "o", "u"].indexOf(word[i]) > -1) {
       //moveConsonants saves the string from position 0 to the position of the first vowel
-      let moveConsonants = word.slice(0,i);
+      let moveConsonants = word.slice(0, i);
       //chunk saves the string from the position of the first vowel to the end of the string
       let chunk = word.slice(i, word.length);
       //puts all of the pieces together
       word = chunk + moveConsonants + "ay";
-    return word;
+      return word;
     }
   }
 }
@@ -71,23 +70,20 @@ const isValid = (word) => {
   //lowercases all of the letters and trims whitespace from the front or the back
   word = word.toLowerCase().trim();
   //checks if string includes a space, hyphen or underscore
-  if(word.includes(" ")||word.includes("-") ||word.includes("_")){
+  if (word.includes(" ") || word.includes("-") || word.includes("_")) {
     return "This pig only latins one word at a time today"
-  }
-  else{
-  //makes sure the word only has letters
-      const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-      let letterChecker = 0
-  for (var i = 0; i < word.length; i++){
-      if(letters.indexOf(word[i]) > -1){
-        letterChecker = letterChecker +1
+  } else {
+    //makes sure the word only has letters
+    const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    let letterChecker = 0
+    for (var i = 0; i < word.length; i++) {
+      if (letters.indexOf(word[i]) > -1) {
+        letterChecker = letterChecker + 1
       }
     }
-    if(letterChecker == word.length){
+    if (letterChecker == word.length) {
       return vowelStartYayEnd(word);
-    }
-    else{
+    } else {
       return "Silly rabbit, pig latin is for letters!"
     }
   }
@@ -95,15 +91,13 @@ const isValid = (word) => {
 
 
 function pigLatin(word) {
-
   return isValid(word)
-
 }
 
 
 function getPrompt() {
   rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
+    console.log(pigLatin(answer));
     getPrompt();
   });
 }
