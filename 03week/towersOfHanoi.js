79491a240907a39832cd97032e6842cadcca7cb8
Nikida Sexton <nikida.ks@gmail.com>
2018-08-03 21:12:30 -0600

    wrote reset function to increase number after every win up to 8 numbers and fixed checkWin function to work for any of the starting numbers

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,27 +19,40 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
+/*Code Plan
+1. Write checkForWin function to check if all of the numbers are in the b row in order
+2. Write isLegal function to check:
+    1. Did the user enter a, b, or c?
+    2. Did the user pick from a row that has a number?
+    3. Did the user choose a smaller number to go on a bigger number?
+3. Write movePiece function to append the last number in startStack to endStack
+4. Figure out what ACA means by reset function and write it
+*/
+
 
 function movePiece(startStack, endStack) {
   stacks[endStack].push(stacks[startStack].pop());
   checkForWin(startStack, endStack);
 }
 
-
 function isLegal(startStack, endStack) {
   if((startStack === "a" || startStack === "b" || startStack === "c")
     && (endStack === "a" || endStack === "b" || endStack === "c")){
-    let startLast = stacks[startStack][stacks[startStack].length-1];
-    let endLast = stacks[endStack][stacks[endStack].length-1];
-    console.log(startLast, endLast);
+    const startLast = stacks[startStack][stacks[startStack].length-1];
+    const endLast = stacks[endStack][stacks[endStack].length-1];
     if(stacks[startStack].length > 0){
-      if(endLast > startLast || !endLast){
-        movePiece(startStack, endStack);
-        return true;
+      if(endLast !== startLast){
+        if(endLast > startLast || !endLast){
+          movePiece(startStack, endStack);
+          return true;
+        }
+        else{
+          console.log("you can't put a larger number on a smaller number")
+          return false
+        }
       }
       else{
-      console.log("you can't put a larger number on a smaller number")
-      return false
+        console.log("you can't put a number on itself")
       }
     }
     else{
@@ -53,23 +66,34 @@ function isLegal(startStack, endStack) {
   }
 }
 
-
-function checkForWin() {
-  if(stacks.b.toString()==="4,3,2,1"){
-    console.log("You win!")
+function resetGame(){
+  if(stacks.b[0] < 9){
+    stacks.b.unshift(stacks.b[0]+1);
+    const reset = stacks.b.slice(0, stacks.b.length);
+    stacks.a.push(reset);
+    stacks.b.length=0;
+  }
+  else{
+    console.log("You are now the King of Hanoi!!!!");
     stacks = {
       a: [4, 3, 2, 1],
       b: [],
-      c: [],
+      c: []
     };
-    return true
-  }
-  else{
-    printStacks();
-    return false
   }
 }
 
+function checkForWin() {
+  if(stacks.a.length == 0 && stacks.c.length == 0){
+    console.log("GREAT JOB!!!!!");
+    console.log("Welcome to the next level");
+    resetGame();
+    return true
+  }
+  else{
+    return false
+  }
+}
 
 function towersOfHanoi(startStack, endStack) {
   isLegal(startStack, endStack);
